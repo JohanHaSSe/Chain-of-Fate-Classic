cc02a85c12c89e2e82cf15a4c56d71ae22288123
Falk Meyer <johanhaseacar@gmail.com>
2025-07-21 01:41:31 +0300

    Little update
    Make a main menu with an animation

--- comands.py
+++ comands.py
@@ -1,5 +1,45 @@
 # Imports
+import os
+import time
 
-class View():
-	pass
+class Actions():
+	def build_table(content: list, lines: int, rows: int):
+		'''Universal table builder'''
+		tsymb = ["═", "╔", "╦", "╗", "╠", "╬", "╣", "╚", "╩", "╝", "║"]
+		
+		# Maximum symbols per every row
+		line = max(len(x) for x in content)
+		
 
+class Rooms():
+	"""Co"""
+
+	def main_menu():
+		"""Function to the load (in progress) or to start a new game (don't have rn)"""
+		game_logo = '''   █████████           ███████████
+  ███░░░░░███         ░░███░░░░░░█
+ ███     ░░░   ██████  ░███   █ ░ 
+░███          ███░░███ ░███████   
+░███         ░███ ░███ ░███░░░█   
+░░███     ███░███ ░███ ░███  ░    
+ ░░█████████ ░░██████  █████      
+  ░░░░░░░░░   ░░░░░░  ░░░░░       
+
+'''
+
+		# Rofl
+		logo = game_logo.split('\n')
+		for line in range(len(logo)):
+			print(logo[line], flush=True)
+			time.sleep(0.05)
+
+		# Building the table of the choises
+
+
+		move = int(input("Welcome to this game! Please, select a game or start a new one: "))
+
+		return move
+
+class Cleaners():
+	clean_windows_screen = lambda: os.system('cls')  # Clear screen for windows users
+	clean_linux_screen = lambda: os.system('clear')  # Clear screen for linux users
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 # Imports
+from comands import Cleaners as clnrs
+from comands import Rooms as r
 
 
 def main():
-	pass
+	game_cycle = True
+
+	while game_cycle:
+		clnrs.clean_windows_screen()
+
+		hellouser = r.main_menu()
+
+		if hellouser == 1:
+			print("Let's Start!")
+		else:
+			print("Goodbye!")
+			break
+
 
 if __name__ == '__main__':
 	main()
 else:
 	print("This code is so ass")
-
